cee1d0fa8ae4a407175d744af711a67fba681898
Jeffrey Tilson <jtilson@renci.org>
2020-12-03 07:52:34 -0500

    Added interval choice

--- get_obs_stations/test/test_predictions.py
+++ get_obs_stations/test/test_predictions.py
@@ -9,31 +9,32 @@ import pandas as pd
 from get_obs_stations.GetObsStations import GetObsStations
 from utilities.utilities import utilities as utilities
 
-#timein = '2020-01-01 00:00'
-#timeout = '2020-01-10 00:06'
+timein = '2020-01-01 00:00'
+timeout = '2020-01-10 00:06'
 
-timein = '2018-01-01 00:00'
-timeout = '2018-12-31 18:00'
+#timein = '2018-01-01 00:00'
+#timeout = '2018-12-31 18:00'
 
 iometadata = ('_'+timein+'_'+timeout).replace(' ','_')
 
 config = utilities.load_config() # Defaults to main.yml as sapecified in the config
 rootdir=utilities.fetchBasedir(config['DEFAULT']['RDIR'], basedirExtra='StationTest')
 
+#rpl = GetObsStations(product='predictions', rootdir=rootdir, yamlname=os.path.join(os.path.dirname(__file__), '../../config', 'obs.yml'), metadata=iometadata)
 rpl = GetObsStations(product='hourly_height', rootdir=rootdir, yamlname=os.path.join(os.path.dirname(__file__), '../../config', 'obs.yml'), metadata=iometadata)
 df_stationNodelist = rpl.fetchStationNodeList()
 stations = df_stationNodelist['stationid'].to_list()
-stations = stations + [999999]
 
 df_stationData, stationNodelist = rpl.fetchStationMetaDataFromIDs(stations)
 
-df_detailed, count_nan, stationlist, excludelist = rpl.fetchStationProductFromIDlist(timein, timeout)
+#df_detailed, count_nan, stationlist, excludelist = rpl.fetchStationProductFromIDlist(timein, timeout)
+df_detailed, count_nan, stationlist, excludelist = rpl.fetchStationProductFromIDlist(timein, timeout,interval='h')
 
-df_pruned, count_nan, newstationlist, excludelist = rpl.fetchStationSmoothedHourlyProductFromIDlist(timein, timeout)
-retained_times = df_pruned.index.to_list() # some may have gotten wacked during the smoothing`
-listSuspectStations = rpl.writeURLsForStationPlotting(newstationlist, timein, timeout)
+#df_pruned, count_nan, newstationlist, excludelist = rpl.fetchStationSmoothedHourlyProductFromIDlist(timein, timeout)
+#retained_times = df_pruned.index.to_list() # some may have gotten wacked during the smoothing`
+#listSuspectStations = rpl.writeURLsForStationPlotting(newstationlist, timein, timeout)
 
-detailedpkl, smoothedpkl, metapkl, urlcsv, exccsv, metaJ, detailedJ, smoothedJ = rpl.fetchOutputNames()
-utilities.log.info('Wrote Station files: Detailed {} Smoothed {} Meta {} URL {} Excluded {} MetaJ {} detailedJ {}, smoothedJ {}'.format(detailedpkl, smoothedpkl, metapkl, urlcsv, exccsv,metaJ, detailedJ, smoothedJ ))
-print('Finished with OBS pipeline')
+#detailedpkl, smoothedpkl, metapkl, urlcsv, exccsv, metaJ, detailedJ, smoothedJ = rpl.fetchOutputNames()
+#utilities.log.info('Wrote Station files: Detailed {} Smoothed {} Meta {} URL {} Excluded {} MetaJ {} detailedJ {}, smoothedJ {}'.format(detailedpkl, smoothedpkl, metapkl, urlcsv, exccsv,metaJ, detailedJ, smoothedJ ))
+#print('Finished with OBS pipeline')
 
