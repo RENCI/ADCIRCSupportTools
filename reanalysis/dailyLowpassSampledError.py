107ce9a58eefd229d69a2a5399fab1d36783ae21
Jeffrey Tilson <jtilson@renci.org>
2021-02-17 14:20:27 -0500

    Includes option to perform stationarity

--- reanalysis/dailyLowpassSampledError.py
+++ reanalysis/dailyLowpassSampledError.py
@@ -21,6 +21,52 @@ from scipy.signal import butter, lfilter, savgol_filter
 import datetime as dt
 from utilities.utilities import utilities
 
+from statsmodels.tsa.stattools import adfuller
+
+def check_stationarity(timeseries): # pd.Series on input
+    # rolling statistics
+    rolling_mean = timeseries.rolling(window=12).mean()
+    rolling_std = timeseries.rolling(window=12).std()
+    # rolling statistics plot
+    original = plt.plot(timeseries, color='blue', label='Original')
+    mean = plt.plot(rolling_mean, color='red', label='Rolling Mean')
+    std = plt.plot(rolling_std, color='black', label='Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+    # Dickey–Fuller test:
+    result = adfuller(timeseries)
+    print('ADF Statistic: {}'.format(result[0]))
+    print('p-value: {}'.format(result[1]))
+    print('Critical Values:')
+    for key, value in result[4].items():
+        print('\t{}: {}'.format(key, value))
+    stats={'ADF Statistic': result[0]}
+    return stats
+
+def make_log_stationary(df_raw,logshift=0.1):
+    """
+    Missing data in df_raw will effectively get interpolated over
+    """
+    global rolling_mean
+    global rmin
+    rmin = np.abs(df_raw.min())
+    df = df_raw+(rmin+logshift)
+    df_log = np.log(df)
+    rolling_mean = df_log.rolling(window=11).mean()
+    df_log_minus_mean = df_log - rolling_mean
+    df_log_minus_mean.dropna(inplace=True)
+    rolling_mean.dropna(inplace=True)
+    #stats=check_stationarity(df_log_minus_mean)
+    return df_log_minus_mean
+
+# Why the extra .loc? because ARIMA drops the first time index value - awesome
+def invert_make_log_stationary(df_log_minus_mean, logshift=0.1):
+    commontimes = df_log_minus_mean.index & rolling_mean.index
+    df_nolog = np.exp(df_log_minus_mean.loc[commontimes]+rolling_mean.loc[commontimes])-rmin-logshift
+    #stats=check_stationarity(df_nolog)
+    return df_nolog
+
 def butter_lowpass_filter(df_data,filterOrder=10, numHours=200):
     """
     Note. Need to drop nans from the data set
@@ -254,9 +300,10 @@ def main(args):
     start = df_err_all.index.min()
 
     # FFT Lowpass each station for entire range time. Then, extract values for all stations every day
-    upshift=4
-    hourly_cutoffs=[168]
-    cutoffs = [x + upshift for x in hourly_cutoffs]
+    upshift=0
+    hourly_cutoff=168
+    cutoff = hourly_cutoff+upshift
+    utilities.log.info('FFT hourly_cutoff {}, actual_cutoff {}'.format(hourly_cutoff,cutoff))
 
     intersectedStations=set(df_err_all.columns.to_list()).intersection(stations) # Compares data to metadata lists
     utilities.log.info('Number of intersected stations is {}'.format(len(intersectedStations)))
@@ -266,21 +313,38 @@ def main(args):
 
     fftAllstations=dict()
     # Perform FFT for each stationm oveer the entire time range
+    if args.stationarity: 
+        utilities.log.info('Will attempt to render data stationary pre FFT')
+
     for station in intersectedStations:
         print('Process station {}'.format(station))
         stationName = df_meta.loc[int(station)]['stationname']
         df_fft=pd.DataFrame()
-        for cutoffflank,cutoff in zip(cutoffs,hourly_cutoffs):
-            print('Process cutoff {} for station {}'.format(cutoff,station))
-            df_temp = df_err_all[station].dropna()
-            df_fft[str(cutoff)]=fft_lowpass(df_temp,lowhrs=cutoffflank)
-        df_fft.index = df_temp.index
+        df_low = pd.DataFrame()
+
+        #for cutoffflank,cutoff in zip(cutoffs,hourly_cutoffs):
+
+        print('Process cutoff {} for station {}'.format(cutoff,station))
+        df_temp = df_err_all[station].dropna()
+        if args.stationarity:
+            df_temp = make_log_stationary(df_temp) # Note beginning times get wiped out 
+        df_low['low'] = fft_lowpass(df_temp,lowhrs=cutoff)
+        df_low.index=df_temp.index # Need this if doing invert_stationarity
+        if args.stationarity:
+            df_inv=invert_make_log_stationary(df_low['low'])
+            df_fft[str(cutoff)] = df_inv
+        else:
+            df_fft[str(cutoff)]=df_low['low']
+        #df_fft.index = df_inv.index # df_temp.index
         df_err_all_lowpass[station]=df_fft[str(cutoff)]
 
     # Now pull out daily data 
-    utilities.log.info('MANUAL setting of date range'))
-    stime=''.join(['2017','-12-20 00:00:00'])
-    etime=''.join(['2019','-01-01 00:00:00'])
+    utilities.log.info('MANUAL setting of date range')
+    #stime=''.join(['2017','-12-20 00:00:00'])
+    #etime=''.join(['2019','-01-01 00:00:00'])
+
+    stime=''.join(['2018','-01-01 00:00:00'])
+    etime=''.join(['2018','-04-01 00:00:00'])
 
     starttime = dt.datetime.strptime(stime,'%Y-%m-%d %H:%M:%S')
     endtime = dt.datetime.strptime(etime,'%Y-%m-%d %H:%M:%S')
@@ -302,6 +366,7 @@ def main(args):
     # Preceding yearly method may have restricted days
 
     # startday is in yyyy-mm-dd lowpass includes times
+    # ALl Nans
     intersect = [value for value in startday if value in df_err_all_lowpass.index] 
 
     utilities.log.info('Residual data: intersect list {}'.format(intersect))
@@ -312,6 +377,7 @@ def main(args):
     # Store the list of filenames into a dict for krig processing
 
     subdir='errorfield'
+
 
     datadict = dict()
     for index, df in df_err_all_lowpass_subselect.iterrows():
@@ -352,6 +418,7 @@ if __name__ == '__main__':
                         help='directory for yearly data')
     parser.add_argument('--iometadata', action='store', dest='iometadata',default='', help='Used to further annotate output files', type=str)
     parser.add_argument('--iosubdir', action='store', dest='iosubdir',default='', help='Used to locate output files into subdir', type=str)
+    parser.add_argument('--stationarity', help="Apply RMean Stationarity pre FFT", action='store_true')
     args = parser.parse_args()
     sys.exit(main(args))
 
