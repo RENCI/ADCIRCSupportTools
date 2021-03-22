a3a53dee5fd3ff9950fe6a8f5f0a4da4df20901f
Jeffrey Tilson <jtilson@renci.org>
2021-03-22 17:24:58 -0400

    Fix misspelled key

---
+++ reanalysis/runInterpolate_parallel_region3.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+export YEAR=2018
+
+export SILL=0.015
+export RANGE=8
+export NUGGET=0.005
+
+#DAILY=DAILY-4MONTH-RANGE$RANGE-SILL$SILL-LP48
+DAILY=DAILY-4MONTH-REGION3-RANGE$RANGE-SILL$SILL-NUGGET$NUGGET-LP48
+
+export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/NEW-ADCIRCSupportTools/ADCIRCSupportTools
+#export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools
+#export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/CausalInference/CausalNetworking_forKirk/TEST/ADCIRCSupportTools
+export PYTHONPATH=$CODEBASE
+export RUNTIMEDIR=.
+export BASEDIREXTRA=REANALYSIS_COMPREHENSIVE/YEARLY-$YEAR
+#export BASEDIREXTRA=REANALYSIS_NOFFT/YEARLY-$YEAR
+
+# Build the yearly error file store in $RUNTIMEDIR/BASEDIREXTRA
+python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
+mv $RUNTIMEDIR/log $RUNTIMEDIR/$BASEDIREXTRA/log-yearly
+
+# Store files in $RUNTIMEDIR/DAILY/errorfield
+export INDIR=$RUNTIMEDIR/$BASEDIREXTRA
+export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
+python dailyLowpassSampledError.py --inDir $INDIR --outroot $OUTROOT # --stationarity
+mv $RUNTIMEDIR/log $OUTROOT/log-daily
+
+# Interpolate a single specific file
+export ADCJSON=$INDIR/adc_coord.json
+export CLAMPFILE=$CODEBASE/config/clamp_list_hsofs.dat
+export YAMLNAME=$CODEBASE/config/int.REANALYSIS.yml
+export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
+export ERRDIR=$OUTROOT/errorfield
+python  runInterpolate_parallel.py  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
+mv $RUNTIMEDIR/log $OUTROOT/log-interpolate
+
