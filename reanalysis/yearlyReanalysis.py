d5b427ccc5e27e85e4c0fc460489df321fe3ccf5
Jeffrey Tilson <jtilson@renci.org>
2021-03-22 12:48:55 -0400

    Correct pointer to obs.yml

--- reanalysis/yearlyReanalysis.py
+++ reanalysis/yearlyReanalysis.py
@@ -204,7 +204,7 @@ def main(args):
 
     # 3) Setup OBS specific YML-resident values
     utilities.log.info('Fetch Observations')
-    obs_yamlname = os.path.join('/home/jtilson/ADCIRCSupportTools', 'config', 'obs.yml')
+    obs_yamlname = os.path.join(os.path.dirname(__file__), '../config', 'obs.yml')
 
     # Grab time Range and tentative station list from the ADCIRC fetch  (stations may still be filtered out)
     timein = timestart.strftime('%Y%m%d %H:%M')
