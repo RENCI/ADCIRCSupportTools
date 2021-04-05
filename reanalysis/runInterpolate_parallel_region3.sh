44f8c884e62ded08d53b0bf4d8d6d73e387e8fab
Jeffrey Tilson <jtilson@renci.org>
2021-04-05 16:19:44 -0400

    Better scenarios

--- reanalysis/runInterpolate_parallel_region3.sh
+++ reanalysis/runInterpolate_parallel_region3.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-export YEAR=2018
+export YEAR=2018DA
 
 export SILL=0.015
 export RANGE=8
 export NUGGET=0.005
 
+# /projects/ees/TDS/Reanalysis/ADCIRC/ERA5/fr3/2018/fort.63.nc
+
 #DAILY=DAILY-4MONTH-RANGE$RANGE-SILL$SILL-LP48
-DAILY=DAILY-4MONTH-REGION3-RANGE$RANGE-SILL$SILL-NUGGET$NUGGET-LP48
+DAILY=DAILY-2018YEAR-12MONTH-REGION3-RANGE$RANGE-SILL$SILL-NUGGET$NUGGET-LP48
 
 export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools
 export PYTHONPATH=$CODEBASE
 export RUNTIMEDIR=.
 export BASEDIREXTRA=REANALYSIS_COMPREHENSIVE_REGION3/YEARLY-$YEAR
-#export BASEDIREXTRA=REANALYSIS_NOFFT/YEARLY-$YEAR
 
 # Build the yearly error file store in $RUNTIMEDIR/BASEDIREXTRA
-python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
+#python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
+python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3_da.json --grid region3
 mv $RUNTIMEDIR/log $RUNTIMEDIR/$BASEDIREXTRA/log-yearly
 
 # Store files in $RUNTIMEDIR/DAILY/errorfield
@@ -32,4 +34,3 @@ export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
 export ERRDIR=$OUTROOT/errorfield
 python  runInterpolate_parallel.py  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
 mv $RUNTIMEDIR/log $OUTROOT/log-interpolate
-
