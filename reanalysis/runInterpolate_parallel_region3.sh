c0efaf8b8698766648af6858a240823fa46a4e96
Jeffrey Tilson <jtilson@renci.org>
2021-04-15 03:20:33 -0400

    Mods to account for alt grids

--- reanalysis/runInterpolate_parallel_region3.sh
+++ reanalysis/runInterpolate_parallel_region3.sh
@@ -12,20 +12,20 @@ export NUGGET=0.005
 #DAILY=DAILY-4MONTH-RANGE$RANGE-SILL$SILL-LP48
 DAILY=DAILY-2018YEAR-12MONTH-REGION3-RANGE$RANGE-SILL$SILL-NUGGET$NUGGET-LP48
 
-export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools
+export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools/reanalysis
 export PYTHONPATH=$CODEBASE
 export RUNTIMEDIR=.
 export BASEDIREXTRA=REANALYSIS_COMPREHENSIVE_REGION3/YEARLY-$YEAR
 
 # Build the yearly error file store in $RUNTIMEDIR/BASEDIREXTRA
-#python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
-python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3_da.json --grid region3
+#python $CODEBASE/yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
+python $CODEBASE/yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3_da.json --grid region3
 mv $RUNTIMEDIR/AdcircSupportTools.log $RUNTIMEDIR/$BASEDIREXTRA/log-yearly
 
 # Store files in $RUNTIMEDIR/DAILY/errorfield
 export INDIR=$RUNTIMEDIR/$BASEDIREXTRA
 export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
-python dailyLowpassSampledError.py --inDir $INDIR --outroot $OUTROOT # --stationarity
+python $CODEBASE/dailyLowpassSampledError.py --inDir $INDIR --outroot $OUTROOT # --stationarity
 mv $RUNTIMEDIR/AdcircSupportTools.log $OUTROOT/log-daily
 
 # Interpolate a single specific file
@@ -34,5 +34,5 @@ export CLAMPFILE=$CODEBASE/config/clamp_list_hsofs.dat
 export YAMLNAME=$CODEBASE/config/int.REANALYSIS.yml
 export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
 export ERRDIR=$OUTROOT/errorfield
-python  runInterpolate_parallel.py  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
+python  $CODEBASE/runInterpolate_parallel.py  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
 mv $RUNTIMEDIR/AdcircSupportTools.log $OUTROOT/log-interpolate
--- reanalysis/runInterpolate_parallel_region3_weekly.sh
+++ reanalysis/runInterpolate_parallel_region3_weekly.sh
@@ -16,19 +16,19 @@ export NUGGET=0.005
 
 WEEKLY=WEEKLY-2018YEAR-12MONTH-REGION3-RANGE$RANGE-SILL$SILL-NUGGET$NUGGET-LP48
 
-export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools
+export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools/reanalysis
 export PYTHONPATH=$CODEBASE
 export RUNTIMEDIR=.
 export BASEDIREXTRA=REANALYSIS_COMPREHENSIVE_REGION3/YEARLY-$YEAR
 
 # Build the yearly error file store in $RUNTIMEDIR/BASEDIREXTRA
-#python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
-#python yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3_da.json --grid region3
+#python $CODEBASE/yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3.json --grid region3
+#python $CODEBASE/yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson region3_da.json --grid region3
 
 # Store files in $RUNTIMEDIR/WEEKLY/errorfield
 export INDIR=$RUNTIMEDIR/$BASEDIREXTRA
 export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$WEEKLY
-python weeklyLowpassSampledError.py --inyear $YEARNUMBER --inDir $INDIR --outroot $OUTROOT
+python $CODEBASE/weeklyLowpassSampledError.py --inyear $YEARNUMBER --inDir $INDIR --outroot $OUTROOT
 mv $RUNTIMEDIR/AdcircSupportTools.log $OUTROOT/log-weekly
 
 # Interpolate a single specific file
@@ -38,5 +38,5 @@ export CLAMPFILE=$CODEBASE/config/clamp_list_hsofs.dat
 export YAMLNAME=$CODEBASE/config/int.REANALYSIS.yml
 export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$WEEKLY
 export ERRDIR=$OUTROOT/errorfield
-python  runInterpolate_parallel_weekly.py  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
+python  $CODEBASE/runInterpolate_parallel_weekly.py  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
 mv $RUNTIMEDIR/AdcircSupportTools.log $OUTROOT/log-interpolate
