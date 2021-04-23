8f4e49c3cf43900c65a3b30b07ecf1a92b77bd2d
Jeffrey Tilson <jtilson@renci.org>
2021-04-23 10:22:11 -0400

    Stratify buildout

---
+++ reanalysis/runInterpolate_parallel_CV.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+export YEAR=2018
+
+export SILL=0.015
+export RANGE=8
+export NUGGET=0.005
+
+#DAILY=DAILY-4MONTH-RANGE$RANGE-SILL$SILL-LP48
+#DAILY=DAILY-4MONTH-RANGE$RANGE-SILL$SILL-NUGGET$NUGGET-LP48
+DAILY=DAILY-DELETEME
+
+export CODEBASE=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools/reanalysis
+export PYTHONPATH=/projects/sequence_analysis/vol1/prediction_work/ADCIRCSupportTools/ADCIRCSupportTools
+export RUNTIMEDIR=.
+export BASEDIREXTRA=REANALYSIS_COMPREHENSIVE/YEARLY-$YEAR
+#export BASEDIREXTRA=REANALYSIS_NOFFT/YEARLY-$YEAR
+
+# Build the yearly error file store in $RUNTIMEDIR/BASEDIREXTRA
+python $CODEBASE/yearlyReanalysis.py --iosubdir $BASEDIREXTRA --urljson reanalysis.json
+mv $RUNTIMEDIR/logs $RUNTIMEDIR/$BASEDIREXTRA/log-yearly
+
+# Store files in $RUNTIMEDIR/DAILY/errorfield
+export INDIR=$RUNTIMEDIR/$BASEDIREXTRA
+export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
+#python $CODEBASE/dailyLowpassSampledError.py --inDir $INDIR --outroot $OUTROOT # --stationarity
+#mv $RUNTIMEDIR/logs $OUTROOT/log-daily
+
+# Interpolate a single specific file
+export ADCJSON=$INDIR/adc_coord.json
+export CLAMPFILE=$CODEBASE/config/clamp_list_hsofs.dat
+export YAMLNAME=$CODEBASE/config/int.REANALYSIS.yml
+export OUTROOT=$RUNTIMEDIR/$BASEDIREXTRA/$DAILY
+export METAFILE=$BASEDIREXTRA/obs_water_level_metadata.json
+export ERRDIR=$OUTROOT/errorfield
+#python  $CODEBASE/runInterpolate_parallel_CV.py --metadataFile $METAFILE  --insill $SILL --inrange $RANGE --outroot $OUTROOT --yamlname $YAMLNAME --errordir $ERRDIR --clampfile $CLAMPFILE --gridjsonfile $ADCJSON
+#mv $RUNTIMEDIR/logs $OUTROOT/log-interpolate
+
